dcdbcc9d5781cd225eeee55f5bfb91c7fb6416aa
Aiden Pickett <Ka7nqk@gmail.com>
2025-03-26 23:05:44 -0600

    Holy smokes made it so that the route positions of each route gets serialized into an integer for storage removed all of the RouteDatabase class and refactored. Still working on it, only have addRoute and getRoute done made SVGImageGenerator have method for a boolean array for ease of use Made various getters and setters for route and binary is now used to parse these things
    bitwise is cool

--- src/main/java/com/aidenPersonal/avyBuddy/RouteFiles/RouteDatabase.java
+++ src/main/java/com/aidenPersonal/avyBuddy/RouteFiles/RouteDatabase.java
@@ -1,12 +1,8 @@
 package com.aidenPersonal.avyBuddy.RouteFiles;
 
-import com.aidenPersonal.avyBuddy.uacData.DataToStringConversions;
-
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * This class manages the connection to the database and allows for adding of routes to
@@ -18,19 +14,19 @@ import java.util.Arrays;
 public class RouteDatabase {
 	
 	//connection string to database
-	static final String url = "";
+	static final String url = "jdbc:sqlite:C:/Users/ka7nq/Desktop/avyBuddy/src/main/resources/database.db";
 	
 	/**
 	 * Writes state of passed route object to database for storage. This includes the name, region, and routePositions
 	 * instance variables values when they are passed
 	 * 
 	 * @param route Route object to have state stored
-	 * @return message representing if route was successfully added or not
+	 * @return true if a route was added, false if not
 	 */
-	public static String addRoute(Route route) {
+	public static boolean addRoute(Route route) {
 		
 		//Checks that route is not already in database
-		if (Arrays.asList(getRouteNames()).contains(route.getName())){
+		if (getRoute(route.getName()) != null) {
 			throw new IllegalArgumentException("The route " + route.getName() + " already exists in the database");
 		}
 		
@@ -40,157 +36,43 @@ public class RouteDatabase {
 			var statement = connection.createStatement();
 			
 			//Writes state of passed Route object to database
-			String sql = "INSERT INTO routes (name, region, route_positions)"
-					+ "VALUES ('" + route.getName() + "', '" + route.getRegion() + "', '" + stringedArray(route.getRoutePositions()) + "')";
+			String sql = "INSERT INTO routes (name, region, routePositions)"
+					+ "VALUES ('" + route.getName() + "', '" + route.getRegion() + "', '" + route.getRoutePositionsBinary() + "')";
 			
 			statement.execute(sql);
 			connection.close();
 			statement.close();
 		} catch (SQLException e) {
-			return "Unsucsessful add, there is already a route named " + route.getName();
+			return false;
 		}
-		return "Sucsessful add, new route " + route.getName() + " has been added.";
+		return true;
 	}
-	
-	/**
-	 * Method that gets the route that corresponds with the name provided
-	 * 
-	 * @param routeName name of the route to get
-	 * @return route object that has same state as one saved in database
-	 */
+
 	public static Route getRoute(String routeName) {
-		
-		ResultSet result;
+		Route returnRoute = null;
+		ResultSet results;
+
 		try {
 			var connection = DriverManager.getConnection(url);
 			var statement = connection.createStatement();
-			
+
 			String sqlName = "SELECT * FROM routes WHERE name = '" + routeName + "';";
-			
-			result = statement.executeQuery(sqlName);
-			
-			String name = result.getString("name");
+			results = statement.executeQuery(sqlName);
+
+			String name = results.getString("name");
+			String region = results.getString("region");
+			int routePositions = results.getInt("routePositions");
 			if(name == null) {
-				throw new IllegalArgumentException("The route " + routeName + " does not exist in the database");
+				return returnRoute;
 			}
-			
-			String region = result.getString("region");
-			boolean[] routePositions = DataToStringConversions.commaSeperatedStringToBooleanArray(result.getString("route_positions"));
-			
-			Route returnRoute = new Route(region, name);
-			returnRoute.addNewRoutePositions(routePositions);
-			
-			connection.close();
-			statement.close();
-			return returnRoute;
+
+			returnRoute = new Route(region, name);
+			returnRoute.setNewRoutePositionsBinary(routePositions);
 		} catch (SQLException e) {
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
-		
-		return null;
+
+		return returnRoute;
 	}
 
-	/**
-	 * Private helper method that converts the routePositions array found in the Route object passed into the addRoute method
-	 * into a comma separated string of 0s (to represent false) and 1s (to represent true) for storage
-	 * 
-	 * @return comma separated String as described above
-	 */
-	private static String stringedArray(boolean[] routePositions) {
-		String returnString = "";
-      for (boolean routePosition : routePositions) {
-          if (routePosition) {
-              returnString += "1, ";
-          } else {
-              returnString += "0, ";
-          }
-      }
-		
-		return returnString.substring(0, returnString.length() - 2);
-	}
-	
-	/**
-	 * This method returns a string-type list of all the names of the routes
-	 * in the database
-	 * 
-	 * @return String-type list described above
-	 */
-	public static String[] getRouteNames() {
-		
-		ArrayList<String> routeNames = new ArrayList<String>();
-		try {
-			var connection = DriverManager.getConnection(url);
-			var statement = connection.createStatement();
-			
-			String sql = "SELECT name FROM routes;";
-			
-			ResultSet results = statement.executeQuery(sql);
-			
-			while(results.next()) {
-				routeNames.add(results.getString("name"));
-			}
-			
-			connection.close();
-			statement.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return routeNames.toArray(new String[0]);
-	}
-	
-	/**
-	 * This method takes a route name and edits its corresponding route positions based
-	 * on the elevation and aspects passed.
-	 * 
-	 * @param routeName name of route to be edited
-	 * @param aspect integer of 0-7, where 0 is north, 1 is northeast, etc
-	 * @param elevation integer of 0-2, where 2 is the lowest elevation band and 0 is the highest
-	 */
-	public static void editRoute(String routeName, int elevation, int...aspect) {
-		
-		Route routeToEdit = getRoute(routeName);
-		routeToEdit.addRouteDanger(elevation, aspect);
-		
-		//Establishes database connection
-		try {
-			var connection = DriverManager.getConnection(url);
-			var statement = connection.createStatement();
-					
-			//Writes new state of Route to route object in database with routeName
-			String sqlChangeRoutePositions = "UPDATE routes SET route_positions = '" + stringedArray(routeToEdit.getRoutePositions()) + "' WHERE name = '" + routeName + "';";
-
-			statement.execute(sqlChangeRoutePositions);
-			
-			connection.close();
-			statement.close();
-		} catch(SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * This method simply deletes a route from the database
-	 * 
-	 * @param routeName Name of route to be deleted
-	 */
-	public static void deleteRoute(String routeName) {
-		
-		//Checks that route exists in database
-		if (getRoute(routeName) != null) {	
-			try {
-				var connection = DriverManager.getConnection(url);
-				var statement = connection.createStatement();
-				
-				String sqlDelete = "DELETE from routes WHERE name = '" + routeName + "';";
-				
-				statement.execute(sqlDelete);
-				
-				connection.close();
-				statement.close();
- 			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			
-		}
-	}
 }
--- src/main/java/com/aidenPersonal/avyBuddy/imageHandling/SvgRoseGenerator.java
+++ src/main/java/com/aidenPersonal/avyBuddy/imageHandling/SvgRoseGenerator.java
@@ -61,6 +61,40 @@ public class SvgRoseGenerator {
     }
 
     /**
+     * This method generates a rose with a given width. The reason that there is a 94 hardcoded
+     * into the creation of the rose is because a rose of width 94 creates no sub-pixel disturbances
+     * that would be noticeable when scaling the .svg.
+     *
+     * @param width desired with of the svg
+     * @param roseValues A 24-length boolean array for the values of each of the aspects/elevations. This is
+     *                   specified by the Routes class
+     * @return A string which can be used as an SVG element in a html file
+     */
+    public static String generateRose(int width, boolean[] roseValues) {
+        if(roseValues.length != 24) {
+            throw new IllegalArgumentException("The number of rose values must be 24");
+        }
+
+        double scaleFactor = (double) width /94;
+        //94 is picked so that the build-in scaling function can be used to get the right width
+        //and scale off of a pixel-perfect model
+        SVGGraphics2D svgGenerator = new SVGGraphics2D((int) (94 * scaleFactor), (int) (94 * scaleFactor));
+        svgGenerator.scale(scaleFactor, scaleFactor);
+
+        int[] roseColors = new int[24];
+        for(int i = 0; i < 24; i++) {
+            if (roseValues[i]) {
+                roseColors[i] = 16;
+            }
+        }
+
+        drawRoseColors(svgGenerator, roseColors);
+        drawRoseOutline(svgGenerator);
+
+        return svgGenerator.getSVGElement();
+    }
+
+    /**
      * This method draws the Colors of the rose. This one is not imprecise, thank God.
      *
      * @param svgGenerator SVGGraphics2D object that the rose will be drawn with

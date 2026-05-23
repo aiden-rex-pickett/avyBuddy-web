51aecdd898ce6c901f043b481f3d37d641fc4dfd
Aiden Pickett <Ka7nqk@gmail.com>
2026-05-22 18:34:10 -0600

    Got account registration page up and running Still some things to iron out but this is a good spot. Also removed annoying command line runner that was testing old code. Good now.

--- src/main/java/com/aidenPersonal/avyBuddy/controllers/AccountController.java
+++ src/main/java/com/aidenPersonal/avyBuddy/controllers/AccountController.java
@@ -1,37 +1,23 @@
 package com.aidenPersonal.avyBuddy.controllers;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import com.aidenPersonal.avyBuddy.services.AccountService;
+
+@Controller
 public class AccountController {
 
-    // TODO: Use environment variable instead here
-    private final String dburl = "jdbc:sqlite:C:/Users/ka7nq/webProjects/avyBuddy/src/main/resources/database.db";
-    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    @Autowired
+    private AccountService accountService;
 
     @PostMapping("/register")
-    public void register(@RequestParam String username, @RequestParam String password) {
-
-        password = encoder.encode(password);
-
-        try {
-            var connection = DriverManager.getConnection(dburl);
-            var statement = connection.createStatement();
-
-            // TODO: Make service and repository and all that to do this the spring way instead
-            String sql = "INSERT INTO accounts (username, password) VALUES ('" + username + "', '" + password + "');";
-
-            statement.execute(sql);
-            connection.close();
-            statement.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public String register(@RequestParam String username, @RequestParam String password) {
+        accountService.addAccount(username, password);
+        // TODO: Change this to using environment variable,
+        // so here it can be localhost and later it can be a domain name
+        return "redirect:http://localhost";
     }
 }
---
+++ src/main/java/com/aidenPersonal/avyBuddy/services/AccountService.java
@@ -0,0 +1,21 @@
+package com.aidenPersonal.avyBuddy.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.aidenPersonal.avyBuddy.models.Account;
+import com.aidenPersonal.avyBuddy.repositories.AccountRepository;;
+
+@Service
+public class AccountService {
+    @Autowired
+    private AccountRepository accountRepo;
+
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public void addAccount(String username, String password) {
+        Account acc = new Account(username, encoder.encode(password));
+        accountRepo.save(acc);
+    }
+}

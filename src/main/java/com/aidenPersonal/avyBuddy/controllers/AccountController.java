05f124074517d6311faec7e3d93084e5f24b878d
Aiden Pickett <Ka7nqk@gmail.com>
2026-05-23 18:29:51 -0600

    Moved to using enviornment variable for domain name

--- src/main/java/com/aidenPersonal/avyBuddy/controllers/AccountController.java
+++ src/main/java/com/aidenPersonal/avyBuddy/controllers/AccountController.java
@@ -1,6 +1,7 @@
 package com.aidenPersonal.avyBuddy.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,14 +11,15 @@ import com.aidenPersonal.avyBuddy.services.AccountService;
 @Controller
 public class AccountController {
 
+    @Value("${DOMAIN}")
+    private String domain;
+
     @Autowired
     private AccountService accountService;
 
     @PostMapping("/register")
     public String register(@RequestParam String username, @RequestParam String password) {
         accountService.addAccount(username, password);
-        // TODO: Change this to using environment variable,
-        // so here it can be localhost and later it can be a domain name
-        return "redirect:http://localhost";
+        return "redirect:http://" + domain;
     }
 }

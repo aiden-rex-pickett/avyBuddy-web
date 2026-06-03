9dc1a7bc55ce9bf858ed74f6488705921c44ed9a
Aiden Pickett <Ka7nqk@gmail.com>
2026-06-02 21:53:12 -0600

    Started hardening. App wont work rn because I need to get forms on the frontend working with csrf

--- src/main/java/com/aidenPersonal/avyBuddy/security/SecurityConfig.java
+++ src/main/java/com/aidenPersonal/avyBuddy/security/SecurityConfig.java
@@ -2,11 +2,14 @@ package com.aidenPersonal.avyBuddy.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -16,16 +19,23 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName(null);
+
         http
-                // TODO: Make so that it only takes requests from the nginx server like only
-                // from the same host
-                .csrf(csrf -> csrf.disable())
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()))
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/forecast", "/getRouteListRecency", "/getRouteListForecast",
                                 "/getRouteListAccount", "/route/*",
                                 "/login", "/register", "/status", "/account/*")
-                        .permitAll()
+                        .access((authentication, context) -> {
+                            String remoteAddress = context.getRequest().getRemoteAddr();
+                            boolean isLocalhost = "127.0.0.1".equals(remoteAddress) || "::1".equals(remoteAddress);
+                            return new AuthorizationDecision(isLocalhost);
+                        })
                         .anyRequest().authenticated())
 
                 .formLogin(form -> form
